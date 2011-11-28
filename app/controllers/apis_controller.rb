fcb85ecb236a7a9a18e8f235d627e56b3e9fec7b
Elena Nazzi <elna@itu.dk>
2011-11-28 15:24:09 +0100

    upload function in api controller

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -397,8 +397,9 @@ class ApisController < ApplicationController
  def upload
   # picture = JSON.parse(params['myfile'])
   #   picture.rewind
+  picture = StringIO.new(Base64.decode64(params['myfile']['tempfile']))
    File.open('public/images/' + params['myfile']['original_filename'], "wb") do |f|
-     f.write(params['myfile']['tempfile'].read)
+     f.write(picture.read)
     end
    render :json => {'response' => 'everything ok, file uploaded'}
   end
