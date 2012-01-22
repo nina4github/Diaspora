2ae9f4eb9c725903aa7327993489f8db68a2b760
bingjie2680 <bingjie2680@gmail.com>
2012-01-22 13:39:19 +0100

    test api v1

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,10 +1,10 @@
 class BaseController
+  
+    protected
     authenticate_with_oauth
     before_filter :set_user_from_oauth
     respond_to :json
-    
-    protected
-    
+
     def set_user_from_oauth
         @user = request.env['oauth2'].resource_owner
     end
