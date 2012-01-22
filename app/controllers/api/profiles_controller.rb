4d3ab090c533d742588fe9ccb19af980106e8c29
bingjie2680 <bingjie2680@gmail.com>
2012-01-22 15:00:17 +0100

    test api v1

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,5 +1,12 @@
 class ProfilesController < BaseController
+    authenticate_with_oauth
+    before_filter :set_user_from_oauth
+    respond_to :json
   
+    def set_user_from_oauth
+        @user = request.env['oauth2'].resource_owner
+    end
+    
     def show
         if params[:id]!=0
             @user=User.find(params[:id])
