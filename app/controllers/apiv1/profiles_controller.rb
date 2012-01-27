6a223634e02a7c8a69bae5b836df0ee842c7164b
bingjie2680 <bingjie2680@gmail.com>
2012-01-27 12:50:30 +0100

    test api v1

--- app/controllers/apiv1/profiles_controller.rb
+++ app/controllers/apiv1/profiles_controller.rb
@@ -2,26 +2,26 @@ class Apiv1::ProfilesController < Apiv1::BaseController
   
     #get a users' profile
     def show
-        if params[:id]!=0
-            @user=Users.find(params[:id])
-        end
-        @person = @user.person
-        profile = @person.profile
-        profiletags = Array.new
-        profile.tags.each do |tag|
-            profiletags << tag.name
-        end
-        @response = { "id"=>profile.id, 
-                      "name" => profile.full_name,
-                      "nichname" => profile.diaspora_handle,
-                      "preferredUsername" =>@user.username,
-                      "bithday"=>profile.birthday,
-                      "gender"=>profile.gender,
-                      "note" => profile.bio,
-                      "picture"=>profile.image_url,
-                      "tags"=>profiletags
-        }
-        render :json => {:actor=>@response }
+      if params[:id]!=0
+          @user=User.find(params[:id])
+      end
+      @person = @user.person
+      profile = @person.profile
+      profiletags = Array.new
+      profile.tags.each do |tag|
+          profiletags << tag.name
+      end
+      @response = { "id"=>profile.id, 
+                    "name" => profile.full_name,
+                    "nichname" => profile.diaspora_handle,
+                    "preferredUsername" =>@user.username,
+                    "bithday"=>profile.birthday,
+                    "gender"=>profile.gender,
+                    "note" => profile.bio,
+                    "picture"=>profile.image_url,
+                    "tags"=>profiletags
+      }
+      render :json => {:actor=>@response }
     end
     
 end
