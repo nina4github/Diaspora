68c79f21831813179b8be9dd6d8787c4e6f05fa8
bingjie2680 <bingjie2680@gmail.com>
2012-01-28 12:09:40 +0100

    test without authentication

--- app/controllers/apiv1/base_controller.rb
+++ app/controllers/apiv1/base_controller.rb
@@ -1,9 +1,10 @@
 class Apiv1::BaseController < ApplicationController
-    authenticate_with_oauth
+    #authenticate_with_oauth
     before_filter :set_user_from_oauth
     respond_to :json
     
     def set_user_from_oauth
-         @user = request.env['oauth2'].resource_owner
+         #@user = request.env['oauth2'].resource_owner
+         @user=User.find_by_username(params[:user])
     end
 end
