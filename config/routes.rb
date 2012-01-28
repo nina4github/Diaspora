4a018bd6940b3261517a90df902eb599e9870fe8
Elena Nazzi <elna@itu.dk>
2012-01-28 16:30:22 +0100

    test fix profile conflict

--- config/routes.rb
+++ config/routes.rb
@@ -166,8 +166,8 @@ Diaspora::Application.routes.draw do
     get "aspects/:aspectname/today" => :today 
     post "aspects/:aspectname/upload" => :upload
     
-    get "aspects/profiles" =>:profiles # get params[:ids] of array of people ids
-    get "aspects/profile" =>:profile  # singular and without parameters retrieve the profile of the current user
+    get "aspects/profiles" =>:theprofiles # get params[:ids] of array of people ids
+    get "aspects/profile" =>:theprofile  # singular and without parameters retrieve the profile of the current user
     get "tags/:tag_name/" =>:tags
     
     post "aspects" => :newaspect
