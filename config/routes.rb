65998bec2a0ab7757c832b9ee5aab44fbd452ee4
bingjie2680 <bingjie2680@gmail.com>
2012-01-30 09:38:25 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -180,7 +180,7 @@ Diaspora::Application.routes.draw do
   #*************api version 1*********************#
 
   namespace :apiv1 do
-      resources :profiles, :aspects, posts, contacts
+      resources :profiles, :aspects, :posts, :contacts
   end
   #match ':controller(/:action(/:id))', :controller => /apiv1\/[^\/]+/
 
