0a4b6cee52da98fe6fa2ae6b459083c924ab68f5
bingjie2680 <bingjie2680@gmail.com>
2012-02-01 16:53:54 +0100

    update profile show method

--- app/controllers/apiv1/profiles_controller.rb
+++ app/controllers/apiv1/profiles_controller.rb
@@ -13,14 +13,14 @@ class Apiv1::ProfilesController < Apiv1::BaseController
             profiletags << tag.name
         end
         @response = { "id"=>profile.id, 
-                      "name" => profile.full_name,
-                      "gender"=>profile.gender,
+                      "firstName" => profile.first_name,
+                      "lastName" => profile.last_name,
                       "description" => profile.bio,
                       "picture"=>profile.image_url,
                       "location"=>profile.location,
-                      "tags"=>profiletags
+                      "catogery"=>profiletags
         }
-        render :json => {:actor=>@response }
+        render :json => {:thing=>@response }
     end   
     
     def edit
