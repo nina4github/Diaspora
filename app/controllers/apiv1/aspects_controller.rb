b70e14020ea95762c082d813d5b424dc18897798
bingjie2680 <bingjie2680@gmail.com>
2012-02-08 12:51:14 +0100

    test

--- app/controllers/apiv1/aspects_controller.rb
+++ app/controllers/apiv1/aspects_controller.rb
@@ -10,7 +10,6 @@ class Apiv1::AspectsController < Apiv1::BaseController
     
     #post a new aspect to the current user
     def create
-		params= params.with_indifferent_access
 		render :json=>params[:aspect]
         #@aspect = @user.aspects.create(params[:aspect])
         
