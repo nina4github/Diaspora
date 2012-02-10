dc867656d821eaf4f42ef86a17d6a17ecfead31b
Elena Nazzi <elna@itu.dk>
2012-02-10 14:42:32 +0100

    updated the apis - renamed back to profile and profiles

--- config/routes.rb
+++ config/routes.rb
@@ -158,6 +158,8 @@ Diaspora::Application.routes.draw do
   
     get :posts
     get :aspects 
+    get "aspects/profiles" =>:profiles # get params[:ids] of array of people ids
+    get "aspects/profile" =>:profile  # singular and without parameters retrieve the profile of the current user
 #    get "aspects/:aspectname" => :stream_old
     get "aspects/:aspectname" => :stream
     get "aspects/:aspectname/contacts" => :contacts
@@ -166,8 +168,7 @@ Diaspora::Application.routes.draw do
     get "aspects/:aspectname/today" => :today 
     post "aspects/:aspectname/upload" => :upload
     
-    get "aspects/profiles" =>:profiles # get params[:ids] of array of people ids
-    get "aspects/profile" =>:profile  # singular and without parameters retrieve the profile of the current user
+
     get "tags/:tag_name/" =>:tags
     
     post "aspects" => :newaspect
