d4c936ab6efe9ccdcce0b23cbdb76be8ad117d5e
Elena Nazzi <elna@itu.dk>
2012-02-16 15:48:10 +0100

    updated apis_controller.upload to return photo, photo path and photo name

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -457,7 +457,7 @@ class ApisController < ApplicationController
     
     
     respond_to do |format|
-      format.json{ render(:layout => false , :json => {"success" => true, "data" => photo}.to_json )}
+      format.json{ render(:layout => false , :json => {"success" => true, "data" => {"photo"=>photo, "path" => photo.remote_photo_name, "name" => photo.remote_photo_path}}.to_json )}
     end
 
 
