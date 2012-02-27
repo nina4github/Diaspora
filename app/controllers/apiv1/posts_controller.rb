395bec1ec774b738669f2117b1d0f5c1846dd814
bingjie2680 <bingjie2680@gmail.com>
2012-02-27 16:10:41 +0100

    add create post method

--- app/controllers/apiv1/posts_controller.rb
+++ app/controllers/apiv1/posts_controller.rb
@@ -7,7 +7,21 @@ class Apiv1::PostsController < Apiv1::BaseController
     end
   
     def create
-        @aspect=theAspect
+        aspect =theAspect()
+        aspect_id = aspect.id
+        # for compatibility with the code of StatusMessagesController.rb
+        params[:status_message][:aspect_ids] = [aspect_id]
+        params[:status_message][:public] = false
+        params[:status_message][:text] = params[:text]
+        current_user=@user
+
+        @status_message = current_user.build_post(:status_message, params[:status_message])    
+        if @status_message.save
+             Rails.logger.info("event=create type=status_message chars=#{params[:status_message][:text].length}")
+             aspects = current_user.aspects_from_ids(params[:status_message][:aspect_ids])
+             current_user.add_to_streams(@status_message, aspects)
+             render :json => {:create =>@status_message.guid, :status => '201'}
+        end
     end
     
 end
