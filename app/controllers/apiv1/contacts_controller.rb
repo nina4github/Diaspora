7f8721117c8a6eebb2d98776d6bb074a10815f77
bingjie2680 <bingjie2680@gmail.com>
2012-03-04 12:07:32 +0100

    update contacts to return user ids

--- app/controllers/apiv1/contacts_controller.rb
+++ app/controllers/apiv1/contacts_controller.rb
@@ -3,18 +3,12 @@ class Apiv1::ContactsController < Apiv1::BaseController
     # GET all contacts within a specific aspect for the current user
     def index
         @contacts = theAspect.contacts
-        @response = Hash.new
         @response=[]
         @contacts.each do |contact|
-            profile= contact.person.profile
-            @profiletags = Array.new
-            profile.tags.each do |tag|
-                @profiletags << tag.name
-            end
-            @response << profile.id 
+            @response << contact.user_id 
         end
         #self id
-        @response << @user.person.profile.id   
+        @response << @user.id   
         render :json =>{
             :contacts=> @response
         }
