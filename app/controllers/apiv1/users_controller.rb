094e973f66fbbdc9d6628c8cb1e7df56d07ac0ce
bingjie2680 <bingjie2680@gmail.com>
2012-03-10 22:32:25 +0100

    update user create

--- app/controllers/apiv1/users_controller.rb
+++ app/controllers/apiv1/users_controller.rb
@@ -7,7 +7,7 @@ class Apiv1::UsersController < Apiv1::BaseController
         user.setup(params)
         if user.save
             user.seed_aspects
-            if(!params[:aspectname].nil)
+            if(!params[:aspectname].nil?)
                 user.aspects.create(:name =>params[:aspectname] )
             end
             render :json=> {:mes => I18n.t('registrations.create.success'), :status => 200 }
