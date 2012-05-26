84f974e2c16888f28a025c536fd1227fcf7237e1
bingjie2680 <bingjie2680@gmail.com>
2012-05-26 12:11:51 +0200

    add update user

--- app/controllers/apiv1/users_controller.rb
+++ app/controllers/apiv1/users_controller.rb
@@ -25,6 +25,38 @@ class Apiv1::UsersController < Apiv1::BaseController
         end
     end
     
+    
+    def update
+      u=params
+      # change email notifications
+      if u[:email_preferences]
+        @user.update_user_preferences(u[:email_preferences])
+        flash[:notice] = I18n.t 'users.update.email_notifications_changed'
+      # change password
+      elsif u[:current_password] && u[:password] && u[:password_confirmation]
+        if @user.update_with_password(u)
+          password_changed = true
+          flash[:notice] = I18n.t 'users.update.password_changed'
+        else
+          flash[:error] = I18n.t 'users.update.password_not_changed'
+        end
+      elsif u[:language]
+        if @user.update_attributes(:language => u[:language])
+          I18n.locale = @user.language
+          flash[:notice] = I18n.t 'users.update.language_changed'
+        else
+          flash[:error] = I18n.t 'users.update.language_not_changed'
+        end
+      elsif u[:email]
+        @user.unconfirmed_email = u[:email]
+        if @user.save
+            render :json => { "text" => "update success", :status=>200 }
+        else
+            render :json => { "text" => "update failed", :status=>500 }
+        end
+      end
+    end
+    
     def destroy
         Resque.enqueue(Jobs::DeleteAccount, @user.id)
         @user.lock_access!
